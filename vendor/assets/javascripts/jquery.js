af2667163faab8fdc27e495fd55adb15aac47843
Nuno Pinge <nuno@pinge.org>
2012-04-29 15:09:38 +0100

    + INSTALL > README > database.yml (default values to match INSTALL)

--- vendor/assets/javascripts/jquery.js
+++ vendor/assets/javascripts/jquery.js
@@ -2,8 +2,8 @@ define([
 
     "order!jquery/jquery-1.7.1",
     "order!jquery/jquery.form-2.94",
-    "order!form2js/src/form2js",
-    "order!form2js/src/jquery.toObject",
+    "order!form2js/form2js",
+    "order!form2js/jquery.toObject",
     "order!jquery/jquery.rparam"
 
 ], function() {
